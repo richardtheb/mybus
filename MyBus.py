124d37b16abe11e6af9f87d5d301e46c57f65368
richardtheb <richard_baguley@baggers.com>
2025-07-23 14:06:44 -0400

    Update MyBus.py

--- MyBus.py
+++ MyBus.py
@@ -4,6 +4,7 @@ import logging
 from datetime import datetime, timezone
 import time
 import os
+import tty
 
 def safe_get_nested_value(data, *keys, default=None):
     """Safely get nested dictionary values"""
